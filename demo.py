685e9c5c90fc86077409712b70035733cc0f0842
Ayan <nalrunyan@gmail.com>
2024-02-17 19:26:52 +0600

    init commit

---
+++ demo.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from src.predictor import HouseModel, HousePricePredictor
+from pickle import load
+from streamlit_authenticator import Authenticate
+
+predictor = HousePricePredictor()
+
+st.title('House Price Prediction Valex')
+
+# Create form for user input
+with st.form("house_input_form"):
+    city = st.text_input('City')
+    district = st.text_input('District')
+    street = st.text_input('Street')
+    house_number = st.text_input('House Number')
+    rooms = st.number_input('Rooms', value=1)
+    square = st.number_input('Square Meters', value=50.0)
+    material = st.selectbox('Material', ['деревянный', 'иное', 'каркасно-камышитовый', 'каркасно-щитовой', 'кирпичный', 'монолитный', 'панельный', 'пеноблочный', 'сэндвич-панели', 'шлакоблочный'])
+    construction_year = st.number_input('Construction Year', min_value=1900, max_value=2024, value=2000)
+    bathroom = st.selectbox('Bathroom', ['2 с/у и более', 'во дворе', 'раздельный', 'совмещенный'])
+    condition = st.selectbox('Condition', ['свободная планировка', 'среднее', 'требует ремонта', 'хорошее', 'черновая отделка'])
+    local_phone = st.selectbox('Local Phone', ['есть', 'есть возможность подключения', 'нет'])
+    internet = st.selectbox('Internet', ['ADSL', 'нет', 'оптика', 'проводной'])
+    sewage = st.selectbox('Sewage', ['есть возможность подведения', 'нет', 'септик', 'центральная'])
+    water = st.selectbox('Water', ['есть возможность подведения', 'нет', 'скважина', 'центральная'])
+    electricity = st.selectbox('Electricity', ['есть', 'есть возможность подключения', 'нет'])
+    heating_system = st.selectbox('Heating System', ['без отопления', 'на газе', 'на жидком топливе', 'на твердом топливе', 'смешанное', 'центральное'])
+    gas_system = st.selectbox('Gas System', ['автономный', 'есть возможность подключения', 'магистральный', 'нет'])
+    security_system = st.selectbox('Security System', ['да', 'нет'])
+    territory_area_m2 = st.number_input('Territory Area (m2)', value=0.0)
+    level = st.number_input('Level', value=1)
+    household_buildings = st.multiselect('Household Buildings', ['да', 'нет'])
+    household_number = st.number_input('Household Number', value=0)
+    furniture = st.selectbox('Furniture', ['полностью', 'частично', 'нет'])
+
+    submitted = st.form_submit_button("Predict Price")
+
+if submitted:
+    house_data = HouseModel(
+        city=city,
+        district=district,
+        street=street,
+        house_number=house_number,
+        rooms=rooms,
+        square=square,
+        material=material,
+        construction_year=construction_year,
+        bathroom=bathroom,
+        condition=condition,
+        local_phone=local_phone,
+        internet=internet,
+        sewage=sewage,
+        water=water,
+        electricity=electricity,
+        heating_system=heating_system,
+        gas_system=gas_system,
+        security_system=security_system,
+        territory_area_m2=territory_area_m2,
+        level=level,
+        household_buildings=household_buildings,
+        household_number=household_number,
+        furniture=furniture
+    )
+    
+    # Predict the house price
+    predicted_price = predictor.predict_price(house_data)
+    st.write(f"Predicted House Price: {predicted_price}")
---
+++ src/predictor.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import xgboost as xgb
+from pydantic import BaseModel
+from typing import List, Optional
+from pickle import load
+
+class HouseModel(BaseModel):
+    city: str
+    district: str
+    street: str
+    house_number: str
+    rooms: int
+    square: float
+    material: str
+    construction_year: float
+    bathroom: str
+    condition: str
+    local_phone: str
+    internet: str
+    sewage: str
+    water: str
+    electricity: str
+    heating_system: str
+    gas_system: str
+    security_system: str
+    territory_area_m2: float
+    level: int
+    household_buildings: Optional[List[str]]
+    household_number: int
+    furniture: str
+
+
+class HousePricePredictor:
+    def __init__(self):
+        self.house_model = load(open('weights/house_model.pkl', 'rb'))
+        self.train_columns = load(open('weights/df_columns.pkl', 'rb'))
+        self.numerics = ['territory_area_m2', 'level', 'square', 'construction_year', 'rooms']
+
+    def predict_price(self, house_data: HouseModel) -> float:
+        df = {k: 0 for k in self.train_columns}
+
+        json_to_send = house_data.model_dump()
+
+        temp = {}
+        for i in ['city', 'district', 'street', 'house_number', 'household_buildings', 'household_number', 'furniture']:
+            temp[i] = json_to_send.get(i)
+            json_to_send.__delitem__(i)
+
+        if temp.get('household_buildings'):
+            if temp.get('household_buildings') in ['да', 'нет']:
+                df.update({'household_buildings_{}'.format(temp.get('household_buildings')): 1})
+
+        df.update({k: v for k, v in json_to_send.items() if k in self.numerics})
+        df.update({k + '_' + str(v): 1 for k, v in json_to_send.items() if k not in self.numerics})
+
+        df = pd.DataFrame(df, index=[0])
+
+        predicted_base_price = self.house_model.predict(xgb.DMatrix(df))[0] * df['square'].values[0]
+
+        # Apply location-based multipliers
+        predicted_base_price = self._apply_location_multipliers(temp, predicted_base_price)
+
+        # Apply other multipliers
+        if temp.get("household_number") > 1:
+            predicted_base_price *= (0.98 - (0.05 * temp.get("household_number")))
+
+        if temp.get("furniture") == "полностью":
+            predicted_base_price *= 1.05
+
+        if temp.get("furniture") == "частично":
+            predicted_base_price *= 1.003
+
+        if temp.get("household_buildings") == "да":
+            predicted_base_price *= 1.03
+
+        return predicted_base_price
+
+    def _apply_location_multipliers(self, temp, base_price):
+        if temp.get("city") == "Алматы":
+            if temp.get("district") in ["Бостандыкский", "Медеуский", "Ауэзовский", "Алмалинский"]:
+                return base_price
+            elif temp.get("district") in ["Алатауский", "Турксибский", "Жетысуский", "Наурызбайский"]:
+                return base_price * 0.9
+            else:
+                return base_price * 0.75
+        elif temp.get("city") in ["Астана", "Шымкент"]:
+            return base_price * 0.65
+        elif "обл." in temp.get("city"):
+            return base_price * 0.50
+        else:
+            return base_price * 0.6
